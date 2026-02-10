126ef3a23757bdf195739c416f825dc786ae0bc8
Adarsh Gupta <adarshgupta2888@gmail.com>
2026-02-10 15:48:52 +0530

    Update 3719_Longest_Balanced_Subarray_I.java

--- CodeOfTheDay/3719_Longest_Balanced_Subarray_I.java
+++ CodeOfTheDay/3719_Longest_Balanced_Subarray_I.java
@@ -22,4 +22,21 @@ class Solution {
         }
         return res;
     }
+    public int longestBalanced(int[] nums) {
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            Set<Integer> evenSet = new HashSet<>();
+            Set<Integer> oddSet = new HashSet<>();
+            for(int j=i;j<nums.length;j++){
+                if(nums[j]%2==0)
+                    evenSet.add(nums[j]);
+                else
+                    oddSet.add(nums[j]);
+
+                if(evenSet.size()==oddSet.size())
+                    res=Math.max(res,j-i+1);
+            }
+        }
+        return res;
+    }
 }
